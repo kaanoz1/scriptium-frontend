c8701b88875037f02286c64354482a4f3c467639
Kaan <147401302+kaanoz1@users.noreply.github.com>
2025-05-17 20:45:26 +0300

    2.4.1: OO amend

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -1,15 +1,28 @@
+import {
+    CONFLICT_HTTP_RESPONSE_CODE,
+    INTERNAL_SERVER_ERROR_HTTP_RESPONSE_CODE, METHOD_NOT_ALLOWED_HTTP_RESPONSE_CODE,
+    NOT_FOUND_HTTP_RESPONSE_CODE,
+    TOO_MANY_REQUEST_HTTP_RESPONSE_CODE,
+    UNAUTHORIZED_HTTP_RESPONSE_CODE
+} from "@/util/utils";
+
 export type Response<T> = {
-  data: T;
+    data: T;
 };
 
 export type ResponseMessage = {
-  message: string;
+    message: string;
 };
 
 
+export type T_NoAuthenticationRequestErrorCode =
+    typeof UNAUTHORIZED_HTTP_RESPONSE_CODE
+    | typeof NOT_FOUND_HTTP_RESPONSE_CODE
+    | typeof TOO_MANY_REQUEST_HTTP_RESPONSE_CODE
+    | typeof INTERNAL_SERVER_ERROR_HTTP_RESPONSE_CODE
+    | typeof CONFLICT_HTTP_RESPONSE_CODE;
 
-export type NoAuthenticationRequestErrorCode = 401 | 404 | 429 | 500;
 
-export type AuthenticationRequestErrorCode =
-  | NoAuthenticationRequestErrorCode
-  | 405;
+export type T_AuthenticationRequestErrorCode = T_NoAuthenticationRequestErrorCode
+    | typeof METHOD_NOT_ALLOWED_HTTP_RESPONSE_CODE;
+
