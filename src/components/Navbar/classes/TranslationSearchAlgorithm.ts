ac38ce9cc02c38b416ca89f72a6a595503996705
kaanoz1 <kaanozarslan1@gmail.com>
2026-09-11 10:09:55 +0300

    adj: Search Methodology (#39)

--- src/components/Navbar/classes/TranslationSearchAlgorithm.ts
+++ src/components/Navbar/classes/TranslationSearchAlgorithm.ts
@@ -2,59 +2,63 @@ import { makeObservable, observable, computed } from "mobx";
 import { SearchAlgorithm } from "@/components/Navbar/classes/SearchAlgorithm";
 
 export class TranslationSearchAlgorithm extends SearchAlgorithm {
-    public readonly key: string = "translation-search";
-    private static _instance: TranslationSearchAlgorithm | null = null;
+  public readonly key: string = "translation-search";
+  private static _instance: TranslationSearchAlgorithm | null = null;
 
-    private _methodology: TranslationSearchMethodology = TranslationSearchMethodology.ContextSearch;
-    private _emphasize: boolean = false;
-    private _filterSameVerse: boolean = false;
+  private _methodology: TranslationSearchMethodology =
+    TranslationSearchMethodology.TextSearch;
+  private _emphasize: boolean = true;
+  private _filterSameVerse: boolean = true;
 
-    public static getInstance(): TranslationSearchAlgorithm {
-        if (this._instance === null)
-            this._instance = new TranslationSearchAlgorithm();
+  public static getInstance(): TranslationSearchAlgorithm {
+    if (this._instance === null)
+      this._instance = new TranslationSearchAlgorithm();
 
-        return this._instance;
-    }
+    return this._instance;
+  }
 
-    private constructor() {
-        super();
+  private constructor() {
+    super();
 
-        makeObservable<TranslationSearchAlgorithm, "_methodology" | "_emphasize" | "_filterSameVerse">(this, {
-            _methodology: observable,
-            _emphasize: observable,
-            _filterSameVerse: observable,
-            methodology: computed,
-            emphasize: computed,
-            filterSameVerse: computed,
-        });
-    }
+    makeObservable<
+      TranslationSearchAlgorithm,
+      "_methodology" | "_emphasize" | "_filterSameVerse"
+    >(this, {
+      _methodology: observable,
+      _emphasize: observable,
+      _filterSameVerse: observable,
+      methodology: computed,
+      emphasize: computed,
+      filterSameVerse: computed,
+    });
+  }
 
-    public get methodology(): TranslationSearchMethodology {
-        return this._methodology;
-    }
+  public get methodology(): TranslationSearchMethodology {
+    return this._methodology;
+  }
 
-    public set methodology(value: TranslationSearchMethodology) {
-        this._methodology = value;
-    }
+  public set methodology(value: TranslationSearchMethodology) {
+    this._methodology = value;
+  }
 
-    public get emphasize(): boolean {
-        return this._emphasize;
-    }
+  public get emphasize(): boolean {
+    return this._emphasize;
+  }
 
-    public set emphasize(value: boolean) {
-        this._emphasize = value;
-    }
+  public set emphasize(value: boolean) {
+    this._emphasize = value;
+  }
 
-    public get filterSameVerse(): boolean {
-        return this._filterSameVerse;
-    }
+  public get filterSameVerse(): boolean {
+    return this._filterSameVerse;
+  }
 
-    public set filterSameVerse(value: boolean) {
-        this._filterSameVerse = value;
-    }
+  public set filterSameVerse(value: boolean) {
+    this._filterSameVerse = value;
+  }
 }
 
 export enum TranslationSearchMethodology {
-    TextSearch,
-    ContextSearch
+  TextSearch,
+  ContextSearch,
 }
